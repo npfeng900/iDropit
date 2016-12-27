5d2de47f472e4b1756c720077f3f40be531e4850
npfeng900 <niupanfeng@Niu-Panfengs-MacbookPro.local>
2016-12-27 23:05:17 +0800

    Init Project

---
+++ iDropit/iDropit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iDropit
+//
+//  Created by Niu Panfeng on 27/12/2016.
+//  Copyright © 2016 NaPaFeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
